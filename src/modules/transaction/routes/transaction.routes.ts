50022159f082a8179dd8c4ef5c256e982c881257
Michael Peter <michaelhpet@gmail.com>
2025-04-15 15:12:46 +0100

    feat: add request validation for transactions crud

--- src/modules/transaction/routes/transaction.routes.ts
+++ src/modules/transaction/routes/transaction.routes.ts
@@ -6,13 +6,19 @@ import {
   updateTransaction,
   deleteTransaction,
 } from "../controllers/transaction.controller"
+import validateRequest from "../../../shared/middleware/validation/request"
+import {
+  createTransactionSchema,
+  getAllTransactionsSchema,
+  updateTransactionSchema,
+} from "../validators/transaction.schema"
 
 const router = Router()
 
-router.post("/", createTransaction)
-router.get("/", getAllTransactions)
+router.post("/", validateRequest(createTransactionSchema), createTransaction)
+router.get("/", validateRequest(getAllTransactionsSchema), getAllTransactions)
 router.get("/:id", getTransactionById)
-router.put("/:id", updateTransaction)
+router.put("/:id", validateRequest(updateTransactionSchema), updateTransaction)
 router.delete("/:id", deleteTransaction)
 
 export default function transactionRoutes(app: Application) {
