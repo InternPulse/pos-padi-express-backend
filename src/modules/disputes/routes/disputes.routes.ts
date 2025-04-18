82bb49b0bdface6374923c7a653687e99720cafb
Kayode <bubykay@gmail.com>
2025-04-18 19:07:42 +0100

    clean-up

--- src/modules/disputes/routes/disputes.routes.ts
+++ src/modules/disputes/routes/disputes.routes.ts
@@ -10,21 +10,6 @@ import {
 
 import validateRequest from "../../../shared/middleware/validation/request"
 import { createDisputeSchema } from "../validators/dispute.schema"
-// import {
-//   createTransaction,
-//   getTransactionById,
-//   getAllTransactions,
-//   updateTransaction,
-//   deleteTransaction,
-//   getTransactionStats,
-//   getAgentTransactionStats,
-// } from "../controllers/transaction.controller"
-// import validateRequest from "../../../shared/middleware/validation/request"
-// import {
-//   createTransactionSchema,
-//   getAllTransactionsSchema,
-//   updateTransactionSchema,
-// } from "../validators/transaction.schema"
 
 const router = Router()
 router.get("/", getAllDisputesController)
@@ -41,21 +26,5 @@ router.post(
   validateRequest(createDisputeSchema, "body"),
   createDisputeController,
 )
-
-// router.post(
-//   "/",
-//   validateRequest(createTransactionSchema, "body"),
-//   createTransaction,
-// )
-// router.get("/", validateRequest(getAllTransactionsSchema), getAllTransactions)
-// router.get("/stats", getTransactionStats)
-// router.get("/agent/:agent_id/stats", getAgentTransactionStats)
-// router.get("/:id", getTransactionById)
-// router.put(
-//   "/:id",
-//   validateRequest(updateTransactionSchema, "body"),
-//   updateTransaction,
-// )
-// router.delete("/:id", deleteTransaction)
 
 export default (app: Application) => app.use("/api/v1/disputes", router)
