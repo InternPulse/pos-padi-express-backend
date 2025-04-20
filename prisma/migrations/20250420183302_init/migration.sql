d87226479c966aa18026306d855a24523ab46318
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-04-20 19:36:56 +0100

    chore(api): modified prisma migration file

--- prisma/migrations/20250420183302_init/migration.sql
+++ prisma/migrations/20250420183302_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE `Transaction` (
     `agent_id` VARCHAR(255) NOT NULL,
     `customer_id` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
+    `reference` VARCHAR(255) NOT NULL,
     `amount` DOUBLE NOT NULL,
     `fee` DOUBLE NOT NULL,
     `type` VARCHAR(191) NOT NULL,
@@ -13,6 +14,20 @@ CREATE TABLE `Transaction` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Disputes` (
+    `id` VARCHAR(191) NOT NULL,
+    `transaction_id` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'Pending',
+    `resolution_notes` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+
+    UNIQUE INDEX `Disputes_transaction_id_key`(`transaction_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
